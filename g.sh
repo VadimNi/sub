0d70840a0e51ca029130206853296fdec98f8d7f
VadimNi <vadim_ni@mail.ru>
2018-01-31 13:13:46 +0000

    initial project version

--- g.sh
+++ g.sh
@@ -2,5 +2,5 @@ git init
 git add .
 git add --all
 git commit -m "initial project version"
-git remote add origin https://github.com/VadimNi/sub.git
-git push -u  origin master
+git remote add origin1 https://github.com/VadimNi/sub.git
+git push -u  origin1 master
